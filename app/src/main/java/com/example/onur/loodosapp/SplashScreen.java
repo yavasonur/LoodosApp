93cf3dfdfc97629b9f2767947551d96cc98805ca
Onur <yavassonur@gmail.com>
2018-07-27 17:58:12 +0300

    First Commit

--- app/src/main/java/com/example/onur/loodosapp/SplashScreen.java
+++ app/src/main/java/com/example/onur/loodosapp/SplashScreen.java
@@ -1,13 +1,15 @@
 package com.example.onur.loodosapp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
     }
 }
